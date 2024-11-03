d126bbb74921feb9fd87398109de8f872e401bfa
thierryH91200 <thierry.hentic@gmail.com>
2024-11-03 11:34:51 +0100

    Initial Commit

---
+++ PegaseUIData/PegaseUIDataApp.swift
@@ -0,0 +1,43 @@
+//
+//  PegaseUIDataApp.swift
+//  PegaseUIData
+//
+//  Created by Thierry hentic on 03/11/2024.
+//
+
+import SwiftUI
+import SwiftData
+import UniformTypeIdentifiers
+
+@main
+struct PegaseUIDataApp: App {
+    var body: some Scene {
+        DocumentGroup(editing: .itemDocument, migrationPlan: PegaseUIDataMigrationPlan.self) {
+            ContentView()
+        }
+    }
+}
+
+extension UTType {
+    static var itemDocument: UTType {
+        UTType(importedAs: "com.example.item-document")
+    }
+}
+
+struct PegaseUIDataMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [VersionedSchema.Type] = [
+        PegaseUIDataVersionedSchema.self,
+    ]
+
+    static var stages: [MigrationStage] = [
+        // Stages of migration between VersionedSchema, if required.
+    ]
+}
+
+struct PegaseUIDataVersionedSchema: VersionedSchema {
+    static var versionIdentifier = Schema.Version(1, 0, 0)
+
+    static var models: [any PersistentModel.Type] = [
+        Item.self,
+    ]
+}
