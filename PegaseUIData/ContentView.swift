92eb4f97a142a1fda030deb754136c636016decf
thierry <thierry.hentic@gmail.com>
2025-04-02 08:51:34 +0200

    Initial Commit

---
+++ PegaseUIData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PegaseUIData
+//
+//  Created by Thierry hentic on 02/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PegaseUIData/PegaseUIDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  PegaseUIDataApp.swift
+//  PegaseUIData
+//
+//  Created by Thierry hentic on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PegaseUIDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
